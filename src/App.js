8bb8d1612d6c4a34b3ee97e7a2f04429ab362a62
uloulou <ulou@ucsd.edu>
2024-04-30 11:25:43 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Navbar from "./components/Navbar";
 
 export default function App() {
+  const [currentPage, setCurrentPage] = useState("about");
+
+  // Function to handle page change
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
-      <About />
-      <Skills />
+      <Navbar onPageChange={handlePageChange} />
+      {currentPage === "about" && <About />}
+      {currentPage === "skills" && <Skills />}
     </main>
   );
 }
