7560cf9be6271572e490b7e495e8a413232fb480
uloulou <ulou@ucsd.edu>
2024-04-30 11:01:02 -0700

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,18 @@
+import React from "react";
+
 export default function Navbar() {
-    // Function to handle click events on links
-    const handleClick = (e) => {
-      // Prevent the default behavior of the anchor tag
-      e.preventDefault();
-      // Get the target element's href attribute
-      const target = e.target.getAttribute("href");
-      // Scroll to the target element smoothly
-      document.querySelector(target).scrollIntoView({
-        behavior: "smooth"
-      });
-    };
-  
-    return (
-      <header className="bg-white md:sticky top-0 z-10">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a href="#about" className="title-font font-medium text-darkblue mb-4 md:mb-0 md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700">
+  return (
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#About" className="mr-5 hover:text-white">
             About
           </a>
-          <a href="#skills" onClick={handleClick} className="mr-5 hover:text-darkpurple">
+          <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
-        </div>
-      </header>
-    );
-  }
+        </nav>
+      </div>
+    </header>
+  );
+}
